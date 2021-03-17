4a1c564db6ab04d0bb3c04964e7eb63ad79d337b
Arshya GHAVAMI <arshyaghavami@Arshyas-MacBook-Air.local>
2021-03-17 08:27:55 -0700

    Created a new view, created an alert, adjusted the image

--- BucketList.swift/ContentView.swift
+++ BucketList.swift/ContentView.swift
@@ -11,43 +11,78 @@ import MapKit
 
 
 struct ContentView: View {
+  
+    @State private var isUnlocked = false
+   @State private var isShowingAlert = false
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Please authenticate yourself to unlock your places."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                
+                DispatchQueue.main.async {
+                    if success {
+                        self.isUnlocked = true
+                    } else {
+                        self.isShowingAlert = true
+                    }
+                }
+            }
+        } else {
+            print("No biometrics")
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    var body: some View {
+        ZStack {
+            if isUnlocked {
+                CombinedMapView()
+                
+            } else {
+                Button("Unlock Places") {
+                    self.authenticate()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+            }
+        }
+        .alert(isPresented: $isShowingAlert) {
+            Alert(title: Text("There's an error"), message: Text("Aunthentication failed"), dismissButton: .default(Text("Ok")))
+        }
+        
+        
+    }
+}
+
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+//    }
+//}
+struct CombinedMapView: View {
     @State private var centerCoordinate = CLLocationCoordinate2D()
     @State private var locations = [CodableMKPointAnnotation]()
     @State private var selectedPlace: MKPointAnnotation?
     @State private var showingPlaceDetails = false
     @State private var showingEditScreen = false
-    @State private var isUnlocked = false
     
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            let reason = "Please authenticate yourself to unlock your places."
-
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-
-                DispatchQueue.main.async {
-                    if success {
-                        self.isUnlocked = true
-                    } else {
-                        // error
-                    }
-                }
-            }
-        } else {
-            // no biometrics
-        }
-    }
-    
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
-    }
-     
     func loadData() {
         let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
-
+        
         do {
             let data = try Data(contentsOf: filename)
             locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
@@ -66,47 +101,39 @@ struct ContentView: View {
         }
     }
     
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
     
     var body: some View {
         ZStack {
-            if isUnlocked {
-            MapView(annotations: locations, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, centerCoordinate: $centerCoordinate)
-                .edgesIgnoringSafeArea(.all)
-            Circle()
-                .fill(Color.blue)
-                .opacity(0.3)
-                .frame(width: 32, height: 32)
-            
-            VStack {
+        MapView(annotations: locations, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, centerCoordinate: $centerCoordinate)
+        Circle()
+            .fill(Color.blue)
+            .opacity(0.3)
+            .frame(width: 32, height: 32)
+        VStack {
+            Spacer()
+            HStack {
                 Spacer()
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        let newLocation = CodableMKPointAnnotation()
-                        newLocation.title = "Example location"
-                        newLocation.coordinate = self.centerCoordinate
-                        self.locations.append(newLocation)
-                        self.selectedPlace = newLocation
-                        self.showingEditScreen = true
-                    }) {
-                        Image(systemName: "plus")
-                    }
-                    .padding()
-                    .background(Color.black.opacity(0.75))
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .clipShape(Circle())
-                    .padding(.trailing)
+                Button(action: {
+                    let newLocation = CodableMKPointAnnotation()
+                    newLocation.title = "Example location"
+                    newLocation.coordinate = self.centerCoordinate
+                    self.locations.append(newLocation)
+                    self.selectedPlace = newLocation
+                    self.showingEditScreen = true
+                }) {
+                    Image(systemName: "plus")
+                        .padding()
+                        .background(Color.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
                 }
-            }
-            } else {
-                Button("Unlock Places") {
-                    self.authenticate()
                 }
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .clipShape(Capsule())
             }
         }
         .alert(isPresented: $showingPlaceDetails) {
@@ -120,13 +147,5 @@ struct ContentView: View {
             }
         }
         .onAppear(perform: loadData)
-
+    }
 }
-}
-
-
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-//    }
-//}
